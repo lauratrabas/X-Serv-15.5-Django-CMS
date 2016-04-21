c6c772011b2a785464eb6e543e47667d1302a01a
Laura Trabas Clavero <lauratc2801@gmail.com>
2016-04-21 17:07:25 +0200

    Entrega

--- cms/views.py
+++ cms/views.py
@@ -6,13 +6,13 @@ from models import Pages
 
 def pagina(request, identificador):
     try:
-        pag = Pages.objets.get(id = int(identificador))
+        pag = Pages.objects.get(id = int(identificador))
         respuesta = pag.page
     except Pages.DoesNotExist:
         respuesta = "No existe en la base de datos"
     return HttpResponse(respuesta)
 def mostrar(resquest):
-    lista=Pages.objets.all()
+    lista=Pages.objects.all()
     respuesta ="<ol>"
     for pag in lista:
         respuesta +='<li><a href="' + str(pag.id) +'">' + pag.name + '</a>'
